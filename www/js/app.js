9b44f6eef49c50aa390f3ef7bfc0df3207007add
Mahendra Mhatre <mahendramhatre@Mahendras-MBP.dkn.io>
2016-05-08 11:27:23 -0700

    add mahi's changes

--- www/js/app.js
+++ www/js/app.js
@@ -4,15 +4,15 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'ngCordova',  'ionic.service.push', 'angular.filter', 'angular-md5'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'ionic.service.core', 'ionic.service.push', 'angular.filter', 'angular-md5', 'ionic.contrib.ui.tinderCards2'])
 .config(['$ionicAppProvider', function($ionicAppProvider) {
-    $ionicAppProvider.identify({
-        app_id: '6c6701d1',
-        api_key: '3cd4690f5e27d8bded761480138742e90d94d0f9f4c1d683',
-        dev_push: true
-    });
+  $ionicAppProvider.identify({
+    app_id: '6c6701d1',
+    api_key: '3cd4690f5e27d8bded761480138742e90d94d0f9f4c1d683',
+    dev_push: true
+  });
 }])
-.run(function($ionicPlatform, $ionicPush) {
+.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -40,6 +40,81 @@ angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 
   });
 })
 
+  .directive('noScroll', function($document) {
+
+    return {
+      restrict: 'A',
+      link: function($scope, $element, $attr) {
+
+        $document.on('touchmove', function(e) {
+          e.preventDefault();
+        });
+      }
+    }
+  })
+
+  .controller('CardsCtrl', function($scope, TDCardDelegate, $timeout,$rootScope) {
+
+    var cardTypes = [
+      { image: 'http://c4.staticflickr.com/4/3924/18886530069_840bc7d2a5_n.jpg' },
+      { image: 'http://c1.staticflickr.com/1/421/19046467146_548ed09e19_n.jpg' },
+      { image: 'http://c1.staticflickr.com/1/278/18452005203_a3bd2d7938_n.jpg' },
+      { image: 'http://c1.staticflickr.com/1/297/19072713565_be3113bc67_n.jpg' },
+      { image: 'http://c1.staticflickr.com/1/536/19072713515_5961d52357_n.jpg' },
+      { image: 'http://c4.staticflickr.com/4/3937/19072713775_156a560e09_n.jpg' },
+      { image: 'http://c1.staticflickr.com/1/267/19067097362_14d8ed9389_n.jpg' }
+    ];
+
+
+    $scope.cards = {
+      master: Array.prototype.slice.call(cardTypes, 0),
+      active: Array.prototype.slice.call(cardTypes, 0),
+      discards: [],
+      liked: [],
+      disliked: [],
+
+    }
+
+    $scope.cardDestroyed = function(index) {
+      $scope.cards.active.splice(index, 1);
+    };
+
+    $scope.addCard = function() {
+      var newCard = cardTypes[0];
+      $scope.cards.active.push(angular.extend({}, newCard));
+
+    }
+
+    $scope.refreshCards = function() {
+      // Set $scope.cards to null so that directive reloads
+      $scope.cards.active = null;
+      $timeout(function() {
+        $scope.cards.active = Array.prototype.slice.call($scope.cards.master, 0);
+      });
+    }
+
+    $scope.$on('removeCard', function(event, element, card) {
+      var discarded = $scope.cards.master.splice($scope.cards.master.indexOf(card), 1);
+      $scope.cards.discards.push(discarded);
+    });
+
+    $scope.cardSwipedLeft = function(index) {
+      console.log('LEFT SWIPE');
+      var card = $scope.cards.active[index];
+      $scope.cards.disliked.push(card);
+    };
+    $scope.cardSwipedRight = function(index) {
+      console.log('RIGHT SWIPE');
+      var card = $scope.cards.active[index];
+      $scope.cards.liked.push(card);
+      $rootScope.like += 1;
+    };
+
+  })
+
+  .controller('CardCtrl', function($scope, TDCardDelegate) {
+
+  })
 .config(function($stateProvider, $urlRouterProvider, $windowProvider) {
   $stateProvider
 
@@ -155,3 +230,5 @@ angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 
     $urlRouterProvider.otherwise('/app/dash');
   }
 });
+
+$rootScope.like=0;
